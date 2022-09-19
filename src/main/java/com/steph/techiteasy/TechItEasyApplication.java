14a13d24eae1aef27bdb470edb5de96228c01ad2
Steph-Roose <steph241291@gmail.com>
2022-09-19 20:27:01 +0200

    poging twee

--- src/main/java/com/steph/techiteasy/TechItEasyApplication.java
+++ src/main/java/com/steph/techiteasy/TechItEasyApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TechItEasyApplication {
+    // Per ongeluk al gepushed naar main waardoor ik geen goede pull request kan aanmaken.
 
     public static void main(String[] args) {
         SpringApplication.run(TechItEasyApplication.class, args);
